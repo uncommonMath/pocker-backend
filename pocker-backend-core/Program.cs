f1613404d57a4530ebf687f8092966b4b30dd6fb
linq.paper <linq.paper@gmail.com>
2020-10-27 04:02:56 +0300

    hello world!

---
+++ pocker-backend-core/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pocker_backend_core
+{
+    internal static class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
