695e37a6bf694a0367916a70bc0062386d174c76
linq.paper <linq.paper@gmail.com>
2020-10-27 10:23:06 +0300

    add docker for heroku

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# Dockerfile
+
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1
+WORKDIR /app
+COPY . .
+RUN dotnet restore
+RUN dotnet publish -c Release -o out
+
+CMD dotnet out/pocker-backend-core.dll "http://*:$PORT/"
--- pocker-backend-core/Program.cs
+++ pocker-backend-core/Program.cs
@@ -1,13 +1,44 @@
 ﻿using System;
+using System.Net;
 
 namespace pocker_backend_core
 {
     internal static class Program
     {
-        // ReSharper disable once UnusedParameter.Local
-        static void Main(string[] args)
+        private static void SimpleListenerExample(string[] prefixes)
         {
-            Console.WriteLine("Hello World!");
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine ("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            HttpListener listener = new HttpListener();
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse response = context.Response;
+                string responseString = "<HTML><BODY><center>Hello world!!!<center></BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer,0,buffer.Length);
+                output.Close();
+            }
+        }
+        
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            SimpleListenerExample(args);
         }
     }
 }
